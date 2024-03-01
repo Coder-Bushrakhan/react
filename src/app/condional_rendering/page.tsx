cb96274a362e1065bf50424aafde2d41405ee9e4
Coder-Bushrakhan <bk.bushra555@gmail.com>
2024-03-01 13:27:25 +0500

    push

---
+++ src/app/condional_rendering/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+
+import { useState } from "react";
+import { Agent } from "http";
+
+export default function Condition() {
+  
+  const [agr,setage]=useState(true)
+// conditional rendering using if else
+  // if (agr){
+//   return("valid age")
+// }
+// else
+// {
+//   return("invalid age")
+// }
+
+
+// 2nd method using ternary operator
+// return(
+//   <div>
+// {
+//   agr?<div>Best age </div> : <div>invalid</div>
+// }
+// </div>
+// )
+
+}
