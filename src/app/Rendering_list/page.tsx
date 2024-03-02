cda4b2836b734a6edd46b7a9a6dcaca4a65714ae
Coder-Bushrakhan <bk.bushra555@gmail.com>
2024-03-02 11:00:29 +0500

    push

--- src/app/Rendering_list/page.tsx
+++ src/app/Rendering_list/page.tsx
@@ -1,22 +1,47 @@
-export default function Page()
+
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
+export default function Rendering()
 {
-    // make array 
-    const people = [{
-        
-       name: 'Creola Katherine Johnson',
-       profession: 'mathematician',},
-          {    name:'Mario José Molina-Pasquel Henríquez',
-       profession:'chemist',}
-       
-];
-// apply map
-const chemists =people.map(person=>
-<li>{person.profession==='chemist'} </li> )
+    // const item=people.map(person=><ul>{person}</ul>)
+const chemist =people.filter(person=>person.profession==="chemist"  )   ////filtering chemist from data
+const chemist_list= chemist.map(person=><li>   
+     <p>
+       <b>{person.name}:</b>
+       {' ' + person.profession + ' '}
+     </p>
+  </li>)  ////maping over chemist
 
 
-    return(
-        <div className="text-center">
-            <ul>{chemists}</ul>
-        </div>
+const math=people.filter(person=>person.profession==="mathematician")
+const mathList=math.map(person=>
+<li>
+   <b> {person.name}</b> <p>{''+person.profession+""}</p></li> )
+
+
+return(
+        // <li>{item}</li>
+
+//  <ul>{chemist_list}
+// </ul> 
+<ul>{mathList}</ul>
+
     )
 }
